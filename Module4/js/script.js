fac6d0526509c9dedb4a8e9d629f87dde181eaee
Yanivridel <yanivridel@gmail.com>
2024-05-18 16:40:26 +0300

    finished assignment3

---
+++ Module4/js/script.js
@@ -0,0 +1,18 @@
+// Object literals and "this"
+var literalCircle = {
+  radius: 10,
+
+  getArea: function () {
+    console.log(this);
+
+    var increaseRadius = function () {
+      this.radius = 20;
+    };
+    increaseRadius();
+    console.log(this.radius);
+
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+};
+
+console.log(literalCircle.getArea());
